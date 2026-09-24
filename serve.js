845ffb26da89a2ee6448584340f89dc817a75231
witosis-gpt <witosis@gmail.com>
2026-09-24 10:17:25 +0700

    feat: add rupiah coin favicon

--- serve.js
+++ serve.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const root = __dirname;
-const types = { '.css': 'text/css', '.html': 'text/html', '.js': 'application/javascript' };
+const types = { '.css': 'text/css', '.html': 'text/html', '.js': 'application/javascript', '.svg': 'image/svg+xml' };
 
 http.createServer((request, response) => {
   const pathname = request.url.split('?')[0];
